46290f350a875db371c0bb5c696c0c0d3419f589
ChristianSA <cjsanagustin172003@gmail.com>
2024-10-20 21:30:40 +0800

    Changing MainWindow.xaml to Login.xaml

--- Human Resources Management System/Login.xaml.cs
+++ Human Resources Management System/Login.xaml.cs
@@ -14,9 +14,9 @@ namespace Human_Resources_Management_System
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
         }
